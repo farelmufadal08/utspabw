7452b637a236ad9442d08ec6fdd9efe4d7d59ebb
farel mufadal <56870694+farelmufadal08@users.noreply.github.com>
2020-04-30 00:01:48 +0700

    Add files via upload

---
+++ index(4).php
@@ -0,0 +1,177 @@
+<!doctype html>
+<html lang="en"id = "home">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+
+<style>
+  section{
+    height: 420px;
+  }
+</style>
+
+    <title>uts pabw</title>
+  </head>
+  <body class="mt-5">
+      
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+      <a class="navbar-brand" href="#home" class="page-scroll">Bantukami</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#about" class="page-scroll">Pembukaan <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#portroi"class="page-scroll">Aksi</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact" class="page-scroll">Kontak</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+
+    <div class="pull-right pr-5">
+      <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          UTS
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="http://ehakcim.farelmufadal.xyz/">LOGIN</a>
+          <a class="dropdown-item" href="http://ehakcim.farelmufadal.xyz/create.php">CREATE</a>
+          <a class="dropdown-item" href="https://github.com/farelmufadal08/utspabw/tree/master">GITHUB</a>
+        </div>
+      </li>
+      </ul>
+    </div>
+
+    </nav>
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container text-center">
+        <img src="Welcoming-Ramadan.jpg"width ="40%" class="rounded-circle img-thumbnail">
+        <h1 class="display-4">MARHABAN YA RAMADHAN</h1>
+        <p class="lead">BANTU KAMI UNTUK BERSEDEKAH</p>
+      </div>
+    </div>
+
+
+    <section id="about" class="about">
+        <div class="container">
+        <div class="row mb-4 mt-5">
+          <div class="col text-center">
+            <h2>Pembukaan</h2>
+          </div>
+
+</div>
+  <div class="row justify-content-center">
+    <div class="col-md-5 text-center">
+<p>Sembako geratis bagi anda yang membutuhkan.
+
+</p>
+
+    </div>
+
+
+  </div>
+    </div>
+    
+  
+    </section>
+
+<section id="portroi" class="portroi bg-light">
+<div class="container-fluid">
+  <div class="row">
+  <div class="col text-center">
+    <h2>Lapangan</h2>
+  </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-4 col-xs-4 ">
+      <div class="card" >
+        <img src="1.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text text-center">Pembagian sembako bersama presiden jokowi</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4 col-xs-4">
+      <div class="card" >
+          <img src="index.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text text-center">Daftarkan diri anda di sembako untuk anda dibulan ramadhan</p>
+          </div>
+      </div>
+    </div>
+
+    <div class="col-md-4 col-xs-4">
+      <div class="card" >
+        <img src="2.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text text-center">Pembagian sembako di gorontalo </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
+<br>
+
+
+<section id="contact" class="contact"  style="margin-top: 500px;">
+  <div class="container">
+    <div class="row mb-4 pt-5" >
+      <div class="col-xl-12 col-md-12 col-xs-12 text-center">
+        <h2>contact us</h2>
+      </div>
+    </div>
+    
+    <div class="row mb-4 pt-5">
+    <div class="col-xl-6 col-md-6 col-xs-6">
+        <div class="card text-white bg-primary mb-3 text-center">
+          <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text">untuk info lebih lanjut ==>></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-6 col-md-6 col-xs-6">
+        <ul class="list-group">
+          <li class="list-group-item"><h1>Bantukami</h1></li>
+          <li class="list-group-item">085234547688</li>
+          <li class="list-group-item">jln.samosir 23</li>
+          <li class="list-group-item">Sedinginan</li>
+          <li class="list-group-item">Indonesia</li>
+        </ul>
+      </div>
+
+
+        
+   </div>
+  </div>
+  </section>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+ 
+      <script src="/img/script.js"></script>
+ 
+ 
+  </body>
+</html>
